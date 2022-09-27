30abdc81d4bacb39e9550ca681027b98db6f30ba
yuzhounaut <86154919+yuzhounaut@users.noreply.github.com>
2022-09-27 09:13:20 +0800

    Update and rename Isolation Forest SHAP Explain.py to SHAP Explain Isolation Forest.py

--- SHAP Explain Isolation Forest.py
+++ SHAP Explain Isolation Forest.py
@@ -10,15 +10,15 @@ DATASET_PATH=r'2000cellsPA20220407 all peaks hepg2.csv'
 cell_data_df= pd.read_csv(DATASET_PATH,low_memory=False)
 cell_data_df.head()
 cell_data_df.columns
-#specify the 990 features column names to be modelled, dependent on your obtained MS features
-to_model_columns=cell_data_df.columns[1:990]
+#specify the features column names to be modelled, drop the first column
+cell_model_df=cell_data_df.iloc[: , 1:]
 
 from sklearn.ensemble import IsolationForest
 CIF=IsolationForest(n_estimators=100, max_samples='auto', \
                         max_features=1.0, bootstrap=False, n_jobs=-1, random_state=42, verbose=0)
-CIF.fit(cell_data_df[to_model_columns])
+CIF.fit(cell_model_df)
 
-pred = CIF.predict(cell_data_df[to_model_columns])
+pred = CIF.predict(cell_model_df)
 cell_data_df['anomaly']=pred
 outliers=cell_data_df.loc[cell_data_df['anomaly']==-1]
 outlier_index=list(outliers.index)
@@ -37,35 +37,35 @@ import shap
 # Create object that can calculate shap values
 explainer = shap.Explainer(CIF)
 # Calculate Shap values
-shap_values = explainer.shap_values(cell_data_df[to_model_columns])
-shap_valuesh = explainer(cell_data_df[to_model_columns])
+shap_values = explainer.shap_values(cell_model_df)
+shap_valuesh = explainer(cell_model_df)
 shap_values_df = pd.DataFrame(shap_values)
 shap_values_df.to_csv('D:/Download/shap_values_df2.csv', index=False)
 
 shap.initjs()
 # Look at the 1st individual and the explanation of values
 #It is recommended to inspect abnormal and normal points
-shap.force_plot(explainer.expected_value, shap_values[0,:], cell_data_df[to_model_columns].iloc[0,:], 
+shap.force_plot(explainer.expected_value, shap_values[0,:], cell_model_df.iloc[0,:], 
                 plot_cmap="DrDb", matplotlib= True)
 # Look at the 12th individual (which is an outlier) and the explanation of values
-shap.force_plot(explainer.expected_value, shap_values[12,:], cell_data_df[to_model_columns].iloc[12,:], 
+shap.force_plot(explainer.expected_value, shap_values[12,:], cell_model_df.iloc[12,:], 
                 plot_cmap="DrDb", matplotlib= True)
 # Look at the 97th individual and the explanation of values
-shap.force_plot(explainer.expected_value, shap_values[97,:], cell_data_df[to_model_columns].iloc[97,:], 
+shap.force_plot(explainer.expected_value, shap_values[97,:], cell_model_df.iloc[97,:], 
                 plot_cmap="DrDb", matplotlib= True)
 # Look at the 98th individual (which is an outlier)  and the explanation of values
-shap.force_plot(explainer.expected_value, shap_values[98,:], cell_data_df[to_model_columns].iloc[98,:], 
+shap.force_plot(explainer.expected_value, shap_values[98,:], cell_model_df.iloc[98,:], 
                 plot_cmap="DrDb", matplotlib= True)
 
 shap.plots.heatmap(shap_valuesh, max_display=10)
 
 #The Decision Plot for Observation 1
-shap.decision_plot(explainer.expected_value, shap_values[0,:], cell_data_df[to_model_columns].iloc[0,:])
+shap.decision_plot(explainer.expected_value, shap_values[0,:], cell_model_df.iloc[0,:])
 
-shap.summary_plot(shap_values, cell_data_df[to_model_columns])
-shap.summary_plot(shap_values, cell_data_df[to_model_columns], plot_type="bar")
+shap.summary_plot(shap_values, cell_model_df)
+shap.summary_plot(shap_values, cell_model_df, plot_type="bar")
 # visualize the first 5 predictions explanations with a dark red dark blue color map.
-shap.force_plot(explainer.expected_value, shap_values[0:5,:], cell_data_df[to_model_columns].iloc[0:5,:],
+shap.force_plot(explainer.expected_value, shap_values[0:5,:], cell_model_df.iloc[0:5,:],
                 plot_cmap="DrDb", show=False)
 
 
