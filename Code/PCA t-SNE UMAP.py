44a39293326e052f666dc8f0bcd8ca28192d9b9a
yuzhounaut <86154919+yuzhounaut@users.noreply.github.com>
2022-10-10 23:13:02 +0800

    Update PCA t-SNE UMAP.py

--- Code/PCA t-SNE UMAP.py
+++ Code/PCA t-SNE UMAP.py
@@ -158,3 +158,89 @@ plt.ylabel('PC2 ({} %)'.format(round(pca_std.explained_variance_ratio_[1] * 100,
 plt.show()
 
 #95% confidence ellipse for PCA plot
+
+
+
+
+#Visualize data using t-SNE.
+print("t-Distributed Stochastic Neighbor Embedding (t-SNE)")
+model = TSNE(learning_rate = 10, n_components = 2, random_state = 42, perplexity = 30)
+tsne = model.fit_transform(X_std)
+tsne_df = pd.DataFrame(data=tsne, columns=['t-SNE1','t-SNE2']).join(labels)
+plt.figure(dpi=300,figsize = (6,6))
+palette = sns.color_palette("Set2", n_colors=n_colors)
+#Attention: the number of colors is the class number
+sns.set_style("white")
+sns.scatterplot(x='t-SNE1',y='t-SNE2',hue='Class',data=tsne_df, palette=palette, linewidth=0.2, s=30, alpha=0.8).set_title('t-SNE')
+
+#Measure execution time for t-SNE
+def tsne_model(X):
+    model = TSNE(learning_rate = 10, n_components = 2, random_state = 42, perplexity = 30)
+    tsne = model.fit_transform(X)
+    return tsne
+from timeit import Timer
+  
+t = Timer(lambda: tsne_model(X_std))
+print(t.timeit(number=1))
+
+#Visualize data using t-SNE after PCA.
+print("t-Distributed Stochastic Neighbor Embedding (t-SNE) on PCA")
+#Attention: For large datasets, keeping the first 100 components, rather than the usual 50 to minmize distortion from the initial PCA.
+X_reduced = PCA(n_components = 100).fit_transform(X_std)
+model = TSNE(learning_rate = 10, n_components = 2, random_state = 42, perplexity = 30)
+tsne_pca = model.fit_transform(X_reduced)
+tsne_pca_df = pd.DataFrame(data=tsne_pca, columns=['t-SNE1','t-SNE2']).join(labels)
+plt.figure(dpi=300,figsize = (6,6))
+palette = sns.color_palette("Set2", n_colors=n_colors)
+#Attention: the number of colors is the class number
+sns.set_style("white")
+sns.scatterplot(x='t-SNE1',y='t-SNE2',hue='Class',data=tsne_pca_df, palette=palette, linewidth=0.2, s=30, alpha=0.8).set_title('t-SNE after PCA')
+
+#Measure execution time for t-SNE after PCA
+def tsne_model_pca(X):
+    X_reduced = PCA(n_components = 100 ).fit_transform(X_std)
+    model = TSNE(learning_rate = 10, n_components = 2, random_state = 42, perplexity = 30)
+    tsne_pca = model.fit_transform(X_reduced)
+    return tsne_pca
+from timeit import Timer
+  
+t = Timer(lambda: tsne_model_pca(X_std))
+print(t.timeit(number=1))
+
+#Visualize data using UMAP.
+print("Uniform Manifold Approximation and Projection (UMAP)")
+model = UMAP(n_neighbors = 50, min_dist = 0.4, n_components = 2, random_state=42)
+umapper = model.fit_transform(X_std)
+umap_df = pd.DataFrame(data=umapper, columns=['UMAP1','UMAP2']).join(labels)
+plt.figure(dpi=300, figsize = (6,6))
+palette = sns.color_palette("Set2", n_colors=n_colors)
+#Attention: the number of colors is the class number
+sns.set_style("white")
+sns.scatterplot(x='UMAP1',y='UMAP2',hue='Class',data=umap_df, palette=palette, linewidth=0.2, s=30, alpha=0.8).set_title('UMAP')
+
+umap_df
+mapper = UMAP(n_neighbors = 50, min_dist = 0.4, n_components = 2).fit(X_std)
+plt.figure(dpi=300,figsize = (6,6))
+#dpi value times figure size values in inches equals to pixel values 6*300=1800
+umap.plot.points(mapper, labels=umap_df.Class, color_key_cmap='Set2', width=1800, height=1800)
+
+umap.plot.connectivity(mapper, show_points=True, width=1800, height=1800)
+umap.plot.connectivity(mapper, edge_bundling='hammer', width=1800, height=1800)
+
+umap.plot.diagnostic(mapper, diagnostic_type='pca', point_size=30, width=1800, height=1800)
+umap.plot.diagnostic(mapper, diagnostic_type='local_dim', point_size=30, width=1800, height=1800)
+umap.plot.diagnostic(mapper, diagnostic_type='neighborhood', point_size=30, width=1800, height=1800)
+
+#Measure execution time for UMAP
+def umap_model(X):
+    model = UMAP(n_neighbors = 50, min_dist = 0.4, n_components = 2)
+    umapvar = model.fit_transform(X)
+    return umapvar
+from timeit import Timer
+  
+t = Timer(lambda: umap_model(X_std))
+print(t.timeit(number=1))
+
+
+
+
